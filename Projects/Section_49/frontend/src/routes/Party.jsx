9c96a7a968a88e3506c358f6ed74b253531f1dc9
vitorgdias <vitorgalves@live.com>
2024-05-03 14:19:05 -0300

    Adding project PartyTime

---
+++ Projects/Section_49/frontend/src/routes/Party.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
+import partyFetch from "../axios/config"
+
+import { useState, useEffect } from "react"
+
+import { useParams, Link, useNavigate } from "react-router-dom"
+
+import useToast from '../hook/useToast'
+
+import './Party.css'
+
+const Party = () => {
+    const {id} = useParams()
+    const [party, setParty] = useState(null)
+
+    const navigate = useNavigate()
+
+    // Delete this party
+    const handleDelete = async () => {
+        const res = await partyFetch.delete(`/parties/${id}`)
+        if(res.status === 200) {
+            navigate("/")
+            useToast(res.data.msg)
+        }
+    }
+
+    // Load Party
+    useEffect(() => {
+        const loadParty = async() => {
+            const res = await partyFetch.get(`/parties/${id}`)
+            console.log(res.data)
+            setParty(res.data)
+        }
+        loadParty()
+    }, [])
+
+    if (!party) return <p>Carregando...</p>
+
+  return (
+    <div className="party">
+        <h1>{party.title}</h1>
+        <div className="actions-container">
+            <Link to={`/party/edit/${party._id}`} className="btn">Editar</Link>
+            <button onClick={handleDelete}  className="btn-secondary">Excluir</button>
+        </div>
+        <p>Orçamento: R${party.budget}</p>
+        <h3>Serviços Contratado:</h3>
+        <div className="services-container">
+            {party.services.map((service) => (
+                <div className="service" key={service._id}>
+                    <img src={service.image} alt={service.name} />
+                    <p>{service.name}</p>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Party
