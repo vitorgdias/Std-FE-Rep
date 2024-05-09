0326802228deefd2cdbaa2d6ce248d0e2f47b940
vitorgdias <vitorgalves@live.com>
2024-05-09 11:48:03 -0300

    TypeScript Intro

---
+++ TypeScript/Section_51/index.ts
@@ -0,0 +1,11 @@
+const firstName = "Vitor"
+const anotherName = 1
+const x = true
+
+function greeting(name: string) {
+    console.log("Olá " + name)
+}
+
+greeting(firstName)
+// greeting(anotherName)
+// greeting(x)
