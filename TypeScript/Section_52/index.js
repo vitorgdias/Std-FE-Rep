c238b7c47c623dc10518601c47f942a57e351e40
vitorgdias <vitorgalves@live.com>
2024-05-09 12:58:15 -0300

    Added TS fundamentals

---
+++ TypeScript/Section_52/index.js
@@ -0,0 +1,43 @@
+"use strict";
+// 1 - Numbers
+let x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+const y = 15.8799;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
+//console.log(y.toUpperCase()) 
+// a própria IDE não dá a sugestão, pois number não possui a função uppercase. A compilação para JS não é possível se houver erros detectados pela IDE
+// 2 - Strings
+const firstName = "Vitor";
+console.log(firstName.toUpperCase());
+let fullName;
+const lastName = "Dias";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+// 3 - Booleans
+let a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
+// 4 - Inference e annotation
+const ann = "Teste";
+let inf = "teste 2";
+// inf = 1 
+// A IDE já identifica que inf é string, conforme definido anteriorment, por isso o TS não deixa prosseguir com a compilação para JS.
+// 5 - Config do TS
+/*
+Gerar um arquivo de config no terminal com  tsc --init
+
+Ao executar o comando, um arquivo json é criado com as opções default.
+*/
+// 6 - Compilação automática do TS
+/*
+Para dar um hot reaload, podemos usar o tsc -w
+Toda vez que salvar o projeto, será feita a compilação.
+*/
+console.log("Testando compilação automática");
