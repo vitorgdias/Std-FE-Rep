8686ab904b34c865ba55420e6ecd97bcb5c5b5b4
vitorgdias <vitorgalves@live.com>
2024-05-10 15:58:45 -0300

    json change

---
+++ Projects/Section_55/multistep/src/components/Thanks.tsx
@@ -0,0 +1,55 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import { ReactElement } from "react";
+
+type ThanksProps = {
+  data: {
+    name: string;
+    review: string;
+    comment: string;
+  };
+};
+
+type emojiObject = {
+  unsatisfied: ReactElement;
+  neutral: ReactElement;
+  satisfied: ReactElement;
+  very_satisfied: ReactElement;
+};
+
+const emojiData: emojiObject = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  very_satisfied: <BsFillEmojiHeartEyesFill />,
+};
+
+import "./Thanks.css";
+
+const Thanks = ({ data }: ThanksProps) => {
+  return (
+    <div className="thanks-container">
+      <h2>Falta pouco</h2>
+      <p>
+        A sua opnião é muito imoportante, em breve você receberá um cupom de 10%
+        de desconto para a sua próxima compra.
+      </p>
+      <p>Para concluir a avaliação clique no botão de Enviar abaixo.</p>
+      <h3>Aqui está o resumo da sua avaliação {data.name}:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto:</span>
+        {emojiData[data.review as keyof typeof emojiData]}
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span> {data.comment}
+      </p>
+    </div>
+  );
+};
+
+export default Thanks;
