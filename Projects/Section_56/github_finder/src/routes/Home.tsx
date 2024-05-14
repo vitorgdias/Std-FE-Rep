636edc8e2ec587f5127fcd0157a1a216642d3cbe
vitorgdias <vitorgalves@live.com>
2024-05-14 08:53:07 -0300

    Finished Github Finder Project

--- Projects/Section_56/github_finder/src/routes/Home.tsx
+++ Projects/Section_56/github_finder/src/routes/Home.tsx
@@ -1,16 +1,31 @@
 import { UserProps } from "../types/user";
 
 import Search from "../components/Search";
+import User from "../components/User";
+import Error from "../components/Error";
+import Loader from "../components/Loader";
 
 import { useState } from "react";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setIsLoading(true);
+    setError(false);
+    setUser(null);
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    setIsLoading(false);
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -27,7 +42,9 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
-      {user && <p>{user.login}</p>}
+      {isLoading && <Loader />}
+      {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
